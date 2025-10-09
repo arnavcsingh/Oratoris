39129996d9906c2d7bd921920f1c10c56fc31ad7
Arnav Singh <sing2415@purdue.edu>
2025-10-09 01:48:44 -0400

    Finished backend routing

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,29 @@
 import express from "express";
-
+import multer from "multer";
+const port = 5100;
 const app = express();
 app.use(express.json());
 app.get('/', (req, res) => {
     res.send("Welcome to Oratoris!")
 });
-app.listen(5100, () => {
-    console.log("App is running on port 5100.")
+const storage = multer.diskStorage({
+    destination: (req, res, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + file.originalname);
+    }
+})
+const upload = multer({ storage });
+app.post('/upload', upload.single('file'), async (req, reactRes) => {
+    const flaskRes = await fetch('http://127.0.0.1:5000/flask');
+    if(flaskRes.ok){
+        const data = await flaskRes.json();
+        reactRes.json({data})
+    } else {
+        reactRes.json(null)
+    }
+})
+app.listen(port, () => {
+    console.log(`App is running on port ${port}.`)
 })
